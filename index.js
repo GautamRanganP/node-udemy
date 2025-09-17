496288c35b4a88d0e088de88d947c63158fa00d1
Gautam rangan P <1000063863@hexaware.com>
2025-09-17 10:00:51 +0530

    Excluding gen AI

--- index.js
+++ index.js
@@ -33,63 +33,81 @@ app.use(cors({
    
 //     next();
 //   };
+const course_ID = [
+5296886,
+2084384,
+5534098,
+5451870,
+6218639,
+5117770,
+6249369,
+6287035,
+5521188,
+5424682,
+6238245,
+6427739,
+3100552
+]
 
-const course_ID = [1219332, 6102963, 5170404, 3829288, 4967038, 5296886, 5281528, 6088953, 5521188, 2084384, 5255276, 5345662, 5117770, 4323374, 5534098, 5474756, 5281486, 5424682, 5451870, 6287035, 6218639, 3100552, 5624824, 6238245, 6249369, 6581503, 5713006, 5609878]
+
 const base64Credentials = Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64');
 // Function to fetch data from Udemy API
-const fetchUdemyData = async (query) => {
-  const url = `${process.env.UDEMY_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
-  const response = await axios.get(url,{
-    headers: {
-      Authorization: `Basic ${base64Credentials}`,
-    },
-  });
-  return response.data;
-};
 // const fetchUdemyData = async (query) => {
-//   const url1 = `${process.env.UDEMY_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
-//   const url2 = `${process.env.UDEMY_COURSE_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
-//   const headers = {
-//     Authorization: `Basic ${base64Credentials}`,
-//   };
-//   // Run both requests in parallel and handle partial failures with allSettled
-//   const results = await Promise.allSettled([
-//     axios.get(url1, { headers }),
-//     axios.get(url2, { headers }),
-//   ]);
-
-//   const response = {
-//     data1: null,
-//     data2: null,
-//     errors: [],
-//   };
-//   let genAIHours = 0
-//   if (results[0].status === "fulfilled") {
-//     response.data1 = results[0].value.data;
-//   } else {
-//     response.errors.push({
-//       url: url1,
-//       message: results[0].reason?.message || String(results[0].reason),
-//     });
-//   }
-//   if (results[1].status === "fulfilled") {
-//     const lookUpIds = new Set(course_ID.map(c => c));
-//     response.data2 = results[1].value.data
-//     let filteredCourses = results[1].value.data.results.filter((course)=> lookUpIds.has(course.course_id));
-//     genAIHours = filteredCourses.reduce((acc,course)=> course.num_video_consumed_minutes + acc  ,0)
-//   } else {
-//     response.errors.push({
-//       url: url2,
-//       message: results[1].reason?.message || String(results[1].reason),
-//     });
-//   }
-//   if(response.data1.results.length > 0){
-//    response.data1.results[0].num_video_consumed_minutes = response.data1.results[0].num_video_consumed_minutes - genAIHours 
-//   }
-  
-//   console.log(genAIHours)
-//   return response.data1;
+//   const url = `${process.env.UDEMY_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
+//   const response = await axios.get(url,{
+//     headers: {
+//       Authorization: `Basic ${base64Credentials}`,
+//     },
+//   });
+//   return response.data;
 // };
+const fetchUdemyData = async (query) => {
+  const url1 = `${process.env.UDEMY_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
+  const url2 = `${process.env.UDEMY_COURSE_API_URL}?from_date=2025-01&to_date=2025-12&user_email=${query}`;
+  const headers = {
+    Authorization: `Basic ${base64Credentials}`,
+  };
+  // Run both requests in parallel and handle partial failures with allSettled
+  const results = await Promise.allSettled([
+    axios.get(url1, { headers }),
+    axios.get(url2, { headers }),
+  ]);
+
+  const response = {
+    data1: null,
+    data2: null,
+    errors: [],
+  };
+  let genAIHours = 0
+  if (results[0].status === "fulfilled") {
+    response.data1 = results[0].value.data;
+    console.log(response.data1)
+  } else {
+    response.errors.push({
+      url: url1,
+      message: results[0].reason?.message || String(results[0].reason),
+    });
+  }
+  if (results[1].status === "fulfilled") {
+    const lookUpIds = new Set(course_ID.map(c => c));
+    response.data2 = results[1].value.data
+    let filteredCourses = results[1].value.data.results.filter((course)=> lookUpIds.has(course.course_id));
+    console.log("filtered",filteredCourses)
+    genAIHours = filteredCourses.reduce((acc,course)=> course.num_video_consumed_minutes + acc  ,0)
+  } else {
+    response.errors.push({
+      url: url2,
+      message: results[1].reason?.message || String(results[1].reason),
+    });
+  }
+  if(response.data1.results.length > 0){
+   let sub = response.data1.results[0].num_video_consumed_minutes - genAIHours 
+   response.data1.results[0].num_video_consumed_minutes = sub > 0 ? sub : 0
+  }
+  
+  console.log(genAIHours)
+  return response.data1;
+};
 app.get("/", async (req, res) => {
   return res.status(200).send("Welcome server up and running");
 })
